7a91977228c2566e83e704441aaa77f771d20a4a
Sahand <76976809+Sahand2020@users.noreply.github.com>
2022-06-21 16:08:29 +0430

    toastify position changed

--- src/components/toast.js
+++ src/components/toast.js
@@ -2,8 +2,8 @@ import { toast } from "react-toastify";
 
 export const notify = (text, type) => {
     if (type === "success") {
-        toast.success(text, { position: "top-left" });
+        toast.success(text, { position: "top-center" });
     } else {
-        toast.error(text, { position: "top-left" });
+        toast.error(text, { position: "top-center" });
     }
 };
